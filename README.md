be820f6994f4b97a59545b06fbeceecdde8ba31d
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2013-09-30 13:41:42 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-angular-timeline
-================
+angular-loader
+==============
 
-Angular directive to display an interactive visualization chart to visualize events in time.
+A really simple angular directive to display a loader image while loading ajax resources, raelying on [Almende Timeline](http://almende.github.io/chap-links-library/timeline.html).
+I've replaced the original range type to display contents on a tooltip when hovering, so you have to rely on Bootstrap tooltips.
+
+Requirements
+============
+
+Include Almende Timeline (CSS, JS) and Bootstrap tooltip (CSS, JS) in your `<head>` section:
+```
+<link rel="stylesheet" href="PATH_TO_STYLES/bootstrap.css">
+<link rel="stylesheet" href="PATH_TO_STYLES/timeline.css">
+
+<script src="PATH_TO_LIB/timeline.js"></script>
+<script src="PATH_TO_LIB/bootstrap-tooltip.js"></script>
+```
+
+Usage
+=====
+Create a div with the ```timeline``` attribute to instantiate a Timeline. Data must be passed as an array of objects, read the [Almende Timeline documentation on available fields](http://almende.github.io/chap-links-library/js/timeline/doc/#Data_Format).
+```
+<div timeline="data" 
+  	 timeline-options="options"
+     timeline-selection="selection"></div>
+```
+
+The ```timeline-options``` attribute provides access to Timeline configuration, read the [Almende Timeline documentation on available options](http://almende.github.io/chap-links-library/js/timeline/doc/#Configuration_Options).
+
+The ```timeline-selection``` attribute provides two-way binding on selected/selecting object.
+
+Credits
+=======
+Credits to [Almende](http://almende.com) providing the excellent [CHAP Links Library](http://almende.github.io/chap-links-library/)
---
+++ timeline.js
@@ -0,0 +1,87 @@
+/**
+ * Angular directive to display an interactive visualization chart to visualize events in time, 
+ * relaying on Almende's Timeline (http://almende.github.io/chap-links-library/timeline.html)
+ * @author Gabriele Destefanis
+ * @version 0.1.0
+ */
+
+'use strict';
+
+
+angular.module('destegabry.timeline', []).
+	.directive('timeline', function() {
+
+		var ItemRangePopup = function(data, options) {
+			links.Timeline.ItemRange.call(this, data, options);
+		};
+
+		ItemRangePopup.prototype = new links.Timeline.ItemRange();
+
+		ItemRangePopup.prototype.updateDOM = function() {
+			var divBox = this.dom;
+			if (divBox) {
+
+				divBox.className = "timeline-event timeline-event-range timeline-event-range-popup ui-widget ui-state-default";
+				divBox.style.height = "22px";
+
+				if (this.content) {
+					$(divBox).tooltip({
+						'placement': 'top',
+						'html': true,
+						'title': this.content,
+						'container': 'body'
+					});
+				}
+
+				if (this.isCluster) {
+					links.Timeline.addClassName(divBox, 'timeline-event-cluster ui-widget-header');
+				}
+
+				if (this.className) {
+					links.Timeline.addClassName(divBox, this.className);
+				}
+			}
+		};
+
+		return {
+			restrict: 'A',
+			scope: {
+				model: '=timeline',
+				options: '=timelineOptions',
+				selection: '=timelineSelection'
+			},
+			link: function($scope, $element) {
+				var timeline = new links.Timeline($element[0]);
+				timeline.addItemType('range', ItemRangePopup);
+
+				links.events.addListener(timeline, 'select', function() {
+					$scope.selection = undefined;
+					var sel = timeline.getSelection();
+					if (sel[0]) {
+						$scope.selection = $scope.model[sel[0].row];
+					}
+				});
+
+				$scope.$watch('model', function(newVal, oldVal) {
+					timeline.setData(newVal);
+				});
+
+				$scope.$watch('options', function(newVal, oldVal) {
+					timeline.draw($scope.model, $scope.options);
+				});
+
+				$scope.$watch('selection', function(newVal, oldVal) {
+					if (!angular.equals(newVal, oldVal)) {
+						for (var i = $scope.model.length - 1; i >= 0; i--) {
+							if (angular.equals($scope.model[i], newVal)) {
+								timeline.setSelection([{
+									row: i
+								}]);
+								break;
+							}
+						};
+					}
+				});
+			}
+		};
+	});
