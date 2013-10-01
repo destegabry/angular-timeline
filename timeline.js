03eff1aa8f809f96ed753a5aa51c0fca94075c01
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2013-10-01 13:43:38 +0100

    Added automatic timeline resize to show all elements when model changes, refactored identation

--- timeline.js
+++ timeline.js
@@ -9,79 +9,80 @@
 
 
 angular.module('destegabry.timeline', [])
-	.directive('timeline', function() {
+  .directive('timeline', function() {
 
-		var ItemRangePopup = function(data, options) {
-			links.Timeline.ItemRange.call(this, data, options);
-		};
+    var ItemRangePopup = function(data, options) {
+      links.Timeline.ItemRange.call(this, data, options);
+    };
 
-		ItemRangePopup.prototype = new links.Timeline.ItemRange();
+    ItemRangePopup.prototype = new links.Timeline.ItemRange();
 
-		ItemRangePopup.prototype.updateDOM = function() {
-			var divBox = this.dom;
-			if (divBox) {
+    ItemRangePopup.prototype.updateDOM = function() {
+      var divBox = this.dom;
+      if (divBox) {
 
-				divBox.className = "timeline-event timeline-event-range timeline-event-range-popup ui-widget ui-state-default";
-				divBox.style.height = "22px";
+        divBox.className = "timeline-event timeline-event-range timeline-event-range-popup ui-widget ui-state-default";
+        divBox.style.height = "22px";
 
-				if (this.content) {
-					$(divBox).tooltip({
-						'placement': 'top',
-						'html': true,
-						'title': this.content,
-						'container': 'body'
-					});
-				}
+        if (this.content) {
+          $(divBox).tooltip({
+            'placement': 'top',
+            'html': true,
+            'title': this.content,
+            'container': 'body'
+          });
+        }
 
-				if (this.isCluster) {
-					links.Timeline.addClassName(divBox, 'timeline-event-cluster ui-widget-header');
-				}
+        if (this.isCluster) {
+          links.Timeline.addClassName(divBox, 'timeline-event-cluster ui-widget-header');
+        }
 
-				if (this.className) {
-					links.Timeline.addClassName(divBox, this.className);
-				}
-			}
-		};
+        if (this.className) {
+          links.Timeline.addClassName(divBox, this.className);
+        }
+      }
+    };
 
-		return {
-			restrict: 'A',
-			scope: {
-				model: '=timeline',
-				options: '=timelineOptions',
-				selection: '=timelineSelection'
-			},
-			link: function($scope, $element) {
-				var timeline = new links.Timeline($element[0]);
-				timeline.addItemType('range', ItemRangePopup);
+    return {
+      restrict: 'A',
+      scope: {
+        model: '=timeline',
+        options: '=timelineOptions',
+        selection: '=timelineSelection'
+      },
+      link: function($scope, $element) {
+        var timeline = new links.Timeline($element[0]);
+        timeline.addItemType('range', ItemRangePopup);
 
-				links.events.addListener(timeline, 'select', function() {
-					$scope.selection = undefined;
-					var sel = timeline.getSelection();
-					if (sel[0]) {
-						$scope.selection = $scope.model[sel[0].row];
-					}
-				});
+        links.events.addListener(timeline, 'select', function() {
+          $scope.selection = undefined;
+          var sel = timeline.getSelection();
+          if (sel[0]) {
+            $scope.selection = $scope.model[sel[0].row];
+          }
+        });
 
-				$scope.$watch('model', function(newVal, oldVal) {
-					timeline.setData(newVal);
-				});
+        $scope.$watch('model', function(newVal, oldVal) {
+          timeline.setData(newVal);
+          timeline.setVisibleChartRangeAuto();
+        });
 
-				$scope.$watch('options', function(newVal, oldVal) {
-					timeline.draw($scope.model, $scope.options);
-				});
+        $scope.$watch('options', function(newVal, oldVal) {
+          timeline.draw($scope.model, $scope.options);
+        });
 
-				$scope.$watch('selection', function(newVal, oldVal) {
-					if (!angular.equals(newVal, oldVal)) {
-						for (var i = $scope.model.length - 1; i >= 0; i--) {
-							if (angular.equals($scope.model[i], newVal)) {
-								timeline.setSelection([{
-									row: i
-								}]);
-								break;
-							}
-						};
-					}
-				});
-			}
-		};
-	});
+        $scope.$watch('selection', function(newVal, oldVal) {
+          if (!angular.equals(newVal, oldVal)) {
+            for (var i = $scope.model.length - 1; i >= 0; i--) {
+              if (angular.equals($scope.model[i], newVal)) {
+                timeline.setSelection([{
+                  row: i
+                }]);
+                break;
+              }
+            };
+          }
+        });
+      }
+    };
+  });
